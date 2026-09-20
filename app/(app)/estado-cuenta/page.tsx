1a984d40b1aa2518d41887516935e47e9b44ac54
UriProtocol <angurisgonur@gmail.com>
2026-09-19 22:17:01 -0600

    placeholder routes

---
+++ app/(app)/estado-cuenta/page.tsx
@@ -0,0 +1,7 @@
+export default function EstadoDeCuenta(){
+    return (
+        <>
+            Estado de cuenta
+        </>
+    )
+}
---
+++ app/(app)/facturas/page.tsx
@@ -0,0 +1,7 @@
+export default function Facturas(){
+    return (
+        <>
+            facturas
+        </>
+    )
+}
