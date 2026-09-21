3dda348a2bbf7d1cdd3e69f2ea1701fc2297dfea
UriProtocol <angurisgonur@gmail.com>
2026-09-21 16:13:12 -0600

    small changes

--- app/components/misc/Divider.tsx
+++ app/components/misc/Divider.tsx
@@ -1,5 +1,5 @@
 export default function Divider({className = ""}: {className?: string}){
     return (
-        <div className={`h-[0.1rem] bg-datia-gray/30 ${className}`}></div>
+        <div className={`h-0.5 bg-datia-primary/10 ${className} max-w-6xl mx-auto`}></div>
     )
 }
