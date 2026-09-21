6b9faf6b498f5cffa58d68159ed67f5cd59cf569
UriProtocol <angurisgonur@gmail.com>
2026-09-21 16:12:03 -0600

    small change

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -6,7 +6,7 @@ import Header from "./header"
 export default function Layout({children}: {children: React.ReactNode}) {
 
     return (
-        <div className=" w-full min-h-screen bg-datia-primary/10">
+        <div className=" w-full min-h-screen bg-datia-primary/7.5">
             <Header />
             <div className="p-8">
                 {children}
