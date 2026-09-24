584943dfaab3d328f3926220149f1739282593d6
UriProtocol <angurisgonur@gmail.com>
2026-09-24 14:15:18 -0600

    vertical divider

--- app/components/misc/Divider.tsx
+++ app/components/misc/Divider.tsx
@@ -1,5 +1,9 @@
-export default function Divider({className = ""}: {className?: string}){
-    return (
-        <div className={`h-0.5 bg-datia-primary/10 ${className} max-w-6xl mx-auto`}></div>
-    )
+export default function Divider({className = "", variant = "horizontal"}: {className?: string, variant?: "vertical" | "horizontal"}){
+
+    if(variant === "horizontal"){
+        return (
+            <div className={`h-0.5 bg-datia-primary/10 ${className} max-w-6xl mx-auto w-full`} />
+        )
+    }
+    return <div className={`w-0.5 shrink-0 self-stretch bg-datia-primary/10 ${className}`} />
 }
