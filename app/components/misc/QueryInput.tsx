edea21b6441c93de4834ad5a4174cf4f5d1923e7
UriProtocol <angurisgonur@gmail.com>
2026-09-24 14:15:06 -0600

    motion div container

--- app/components/misc/QueryInput.tsx
+++ app/components/misc/QueryInput.tsx
@@ -5,6 +5,7 @@ import React, { forwardRef, useState } from 'react'
 import { FaSearch } from 'react-icons/fa';
 import { FaXmark } from 'react-icons/fa6';
 import { useDebouncedCallback } from 'use-debounce';
+import {motion} from 'framer-motion'
 
 export default forwardRef(function QueryInput({ placeholder }: { placeholder?: string }, ref: React.ForwardedRef<HTMLInputElement>) {
 
@@ -32,32 +33,37 @@ export default forwardRef(function QueryInput({ placeholder }: { placeholder?: s
     };
 
     return (
-        <TextField className="w-full" value={query} onChange={e =>{
-            handleSearch(e)
-            setQuery(e)
-        }}>
-            <InputGroup>
-                <InputGroup.Prefix>
-                    <FaSearch className=' text-datia-primary/50'/>
-                </InputGroup.Prefix>
-                <InputGroup.Input
-                    ref={ref}
-                    placeholder={placeholder || "Buscar..."}
-                    className="w-full"
-                />
-                <InputGroup.Suffix>
-                    <Button 
-                        isIconOnly 
-                        className={clsx(
-                            "bg-transparent text-datia-primary/50 -mr-3 transition-all",
-                            !searchParams.get("query") && "opacity-0 pointer-events-none"
-                        )}
-                        onPress={handleClear}
-                    >
-                        <FaXmark />
-                    </Button>
-                </InputGroup.Suffix>
-            </InputGroup>
-        </TextField>
+        <motion.div
+            layout
+            className='w-full'
+        >
+            <TextField className="w-full" value={query} onChange={e =>{
+                handleSearch(e)
+                setQuery(e)
+            }}>
+                <InputGroup>
+                    <InputGroup.Prefix>
+                        <FaSearch className=' text-datia-primary/50'/>
+                    </InputGroup.Prefix>
+                    <InputGroup.Input
+                        ref={ref}
+                        placeholder={placeholder || "Buscar..."}
+                        className="w-full"
+                    />
+                    <InputGroup.Suffix>
+                        <Button 
+                            isIconOnly 
+                            className={clsx(
+                                "bg-transparent text-datia-primary/50 -mr-3 transition-all",
+                                !searchParams.get("query") && "opacity-0 pointer-events-none"
+                            )}
+                            onPress={handleClear}
+                        >
+                            <FaXmark />
+                        </Button>
+                    </InputGroup.Suffix>
+                </InputGroup>
+            </TextField>
+        </motion.div>
     )
 })
